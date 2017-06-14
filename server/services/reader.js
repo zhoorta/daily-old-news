c08d9789e214e2500e9ce9bf128032b0dfbffe16
jmendonca73 <jmendonca@gmail.com>
2017-06-14 17:58:26 +0100

    update

--- server/services/reader.js
+++ server/services/reader.js
@@ -3,14 +3,14 @@ exports.get_nyt_articles = function(res, date, page) {
 	const mongoose = require('mongoose');
  
 	const axios = require('axios');
-	const API = 'http://localhost:3000/api/docs';
+	const API = 'http://don.ironsoft.net/api/docs';
 	
-	mongoose.connect('mongodb://localhost/nyt/docs');
+	mongoose.connect('mongodb://localhost/don');
 	mongoose.Promise = global.Promise;	
 
 	var db = mongoose.connection;
 
-	var Document = require('../models/edition');
+	var Publication = require('../models/publication');
 
 	db.on('error', console.error.bind(console, 'connection error:'));
 	db.once('open', function() {
@@ -42,7 +42,7 @@ exports.get_nyt_articles = function(res, date, page) {
 		var idx = Number(year.toString() + month + day);
 
 
-		Document.findOne({ "id" : idx  }).exec( function(err, docs) {
+		Publication.findOne({ "id" : idx  }).exec( function(err, pub) {
 		
 			console.log('returning results!')
 			if (err) throw err;
@@ -50,7 +50,8 @@ exports.get_nyt_articles = function(res, date, page) {
 	    	db.close();
 	    	mongoose.disconnect();
 
-			res.status(200).json(docs.articles);
+	    	if(pub)	res.status(200).json(pub.articles);
+	    	else res.status(200).json([]);
 
 		});
 
